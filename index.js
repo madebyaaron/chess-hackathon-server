fddecb744212601986e76ed5efe71e505217334b
Aaron Moore <hi@madebyaaron.xyz>
2022-05-28 11:53:01 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import { ApolloServer, gql } from "apollo-server"
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    }
+]
+
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+});
+  
+
+server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+});
